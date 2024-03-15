7b968e7910288d302968f728a4f647c29e5be256
Cuyeshi Yunkay <cuyeshi@mail.ru>
2024-03-15 18:36:41 +0300

    Добавьте файлы проекта.

---
+++ XmlSaxWriting_Example/XmlSaxWriting/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace XmlSaxWriting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // a collection of test users
+            List<User> users = new List<User>();
+            users.Add(new User("John Smith", 22, new DateTime(2000, 3, 21)));
+            users.Add(new User("James Brown", 31, new DateTime(2016, 10, 30)));
+            users.Add(new User("Tom Hanks", 16, new DateTime(2011, 1, 12)));
+
+            // the XmlWriter settings
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+
+            // writes the users
+            using (XmlWriter xw = XmlWriter.Create(@"file2.xml", settings))
+            {
+                xw.WriteStartDocument(); // the header
+                xw.WriteStartElement("users"); // opens the root users element
+
+                // writes individual users
+                foreach (User u in users)
+                {
+                    xw.WriteStartElement("user");
+                    xw.WriteAttributeString("age", u.Age.ToString());
+
+                    xw.WriteElementString("name", u.Name);
+                    xw.WriteElementString("registered", u.Registered.ToShortDateString());
+                    xw.WriteEndElement();
+
+                }
+
+                xw.WriteEndElement(); // closes the root element
+                xw.WriteEndDocument(); // closes the document
+                xw.Flush();
+            }
+
+            using (XmlReader xr = XmlReader.Create(@"file.xml"))
+            {
+                string name = "";
+                int age = 0;
+                DateTime registered = DateTime.Now;
+                string element = "";
+                while (xr.Read())
+                {
+                    // reads the element
+                    if (xr.NodeType == XmlNodeType.Element)
+                    {
+                        element = xr.Name; // the name of the current element
+                        if (element == "user")
+                        {
+                            age = int.Parse(xr.GetAttribute("age"));
+                        }
+                    }
+                    // reads the element value
+                    else if (xr.NodeType == XmlNodeType.Text)
+                    {
+                        switch (element)
+                        {
+                            case "name":
+                                name = xr.Value;
+                                break;
+                            case "registered":
+                                registered = DateTime.Parse(xr.Value);
+                                break;
+                        }
+                    }
+                    /* reads the closing element
+                    else if ((xr.NodeType == XmlNodeType.EndElement) && (xr.Name == "user"))
+                        users.Add(new User(name, age, registered));*/
+                }
+
+                // printing the loaded objects
+                foreach (User u in users)
+                {
+                    Console.WriteLine(u);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ XmlSaxWriting_Example/XmlSaxWriting/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlSaxWriting
+{
+    internal class User
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public DateTime Registered { get; private set; }
+
+        public User(string name, int age, DateTime registered)
+        {
+            Name = name;
+            Age = age;
+            Registered = registered;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}", Name, Age, Registered.ToShortDateString());
+        }
+    }
+}
